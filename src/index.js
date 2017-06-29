dabe55fe42c6676552bfc34fe338ba4e0e70f8cf
Chris Driscol <drizkol@gmail.com>
2017-06-29 15:04:27 -0600

    Fixing lint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class Center extends React.Component {
+	propTypes = {
+		children: PropTypes.object.isRequired
+	}
+
   render() {
       return (
         <center>{this.props.children}</center>
